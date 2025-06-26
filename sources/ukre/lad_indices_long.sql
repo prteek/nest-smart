1910a88e02449ea6792e22c27ab6dcf71392674c
prteek <prteek@icloud.com>
2025-06-26 17:25:13 +0100

    filtering data for better performance

--- sources/ukre/lad_indices_long.sql
+++ sources/ukre/lad_indices_long.sql
@@ -1,3 +1,4 @@
 select *
 from ukre.combined_indices_long
 where area_type = 'lad'
+and ((type='rent' and asset_class in ('flat', 'terraced', 'all')) or (type='buy' and asset_class in ('flat', 'terraced', 'all')))
