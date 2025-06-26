839723f8c9e4d8308580d55ea236e73360b982ec
prteek <prteek@icloud.com>
2025-06-26 17:33:42 +0100

    limiting even more data

--- sources/ukre/region_indices_long.sql
+++ sources/ukre/region_indices_long.sql
@@ -2,4 +2,4 @@ select *
 from ukre.combined_indices_long
 where area_type = 'region'
 and asset_class in ('flat', 'terraced', 'all')
-and time_period >= cast('2020-01-01' as date)
+and time_period >= cast('2024-01-01' as date)
