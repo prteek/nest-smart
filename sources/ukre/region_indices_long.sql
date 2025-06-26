43e495eef0d2d8870f9249b9be852db88268766d
prteek <prteek@icloud.com>
2025-06-26 17:27:23 +0100

    limited asset classes for performance

--- sources/ukre/region_indices_long.sql
+++ sources/ukre/region_indices_long.sql
@@ -1,3 +1,4 @@
 select *
 from ukre.combined_indices_long
 where area_type = 'region'
+and asset_class in ('flat', 'terraced', 'all')
--- sources/ukre/uk_indices_long.sql
+++ sources/ukre/uk_indices_long.sql
@@ -1,3 +1,4 @@
 select *
 from ukre.combined_indices_long
 where area_name = 'united kingdom'
+and asset_class in ('flat', 'terraced', 'all')
