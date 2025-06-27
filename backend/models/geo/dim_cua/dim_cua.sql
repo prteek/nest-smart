164fe68f7f5a0f16b70727976f53960783cb9179
prteek <prteek@icloud.com>
2025-06-27 20:39:28 +0100

    adding cua

---
+++ backend/models/geo/dim_cua/dim_cua.sql
@@ -0,0 +1,12 @@
+select
+    cua,
+    cua_name,
+    st_astext(geometry) AS cua_polygon_wkt,
+    st_astext(st_centroid(geometry)) AS cua_centroid_wkt
+from (
+    select
+        lower(CTYUA24CD) AS cua,
+        lower(CTYUA24NM) AS cua_name,
+        st_transform(geom, 'EPSG:4326', 'EPSG:4326') AS geometry  -- Source CRS already correct
+    from {{ source('geojsons', 'Counties_and_Unitary_Authorities_December_2024_Boundaries_UK_BFC_3348038940373313033.geojson') }}
+)
