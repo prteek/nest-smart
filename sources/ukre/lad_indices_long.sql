87b0ce54e4aa1b5235d44f2371d365bed0799120
prteek <prteek@icloud.com>
2025-06-27 07:57:45 +0100

    using all asset classes

--- sources/ukre/lad_indices_long.sql
+++ sources/ukre/lad_indices_long.sql
@@ -1,5 +1,5 @@
 select *
 from ukre.combined_indices_long
 where area_type = 'lad'
-and asset_class in ('flat', 'terraced', 'all')
+-- and asset_class in ('flat', 'terraced', 'all')
 and time_period >= cast('2020-01-01' as date)
