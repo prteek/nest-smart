f45487a0c21a3977f778e21ec35a82126b317df5
prteek <prteek@icloud.com>
2025-06-28 23:33:36 +0100

    parry up/down

--- backend/models/geo/post_town_geo_mapping/post_town_geo_mapping.sql
+++ backend/models/geo/post_town_geo_mapping/post_town_geo_mapping.sql
@@ -1,4 +1,6 @@
-select post_town
+select cua
+, lad
+, post_town
 , msoa
 , lsoa
 from {{ ref('dim_post_town') }} a
@@ -6,3 +8,7 @@ from {{ ref('dim_post_town') }} a
                 on st_within(ST_GeomFromText(b.msoa_centroid_wkt), ST_GeomFromText(a.post_town_polygon_wkt))
         inner join {{ ref('dim_lsoa') }} c
                 on st_within(ST_GeomFromText(c.lsoa_centroid_wkt), ST_GeomFromText(b.msoa_polygon_wkt))
+        inner join {{ ref('dim_lad') }} d
+                on st_within(ST_GeomFromText(a.post_town_centroid_wkt), ST_GeomFromText(d.lad_polygon_wkt))
+        inner join {{ ref('dim_cua') }} e
+                on st_within(ST_GeomFromText(d.lad_centroid_wkt), ST_GeomFromText(e.cua_polygon_wkt))
