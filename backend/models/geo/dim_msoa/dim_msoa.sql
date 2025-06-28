04c5be03e71fb77f743028cd261703fb0e45fc2b
prteek <prteek@icloud.com>
2025-06-28 23:49:27 +0100

    flip co ordinates to lon,lat

--- backend/models/geo/dim_msoa/dim_msoa.sql
+++ backend/models/geo/dim_msoa/dim_msoa.sql
@@ -7,6 +7,6 @@ from (
     select
         lower(MSOA21CD) AS msoa,
         lower(MSOA21NM) AS msoa_name,
-        st_transform(geom, 'EPSG:27700', 'EPSG:4326') AS geometry  -- Source CRS needs conversion
+        ST_FlipCoordinates(st_transform(geom, 'EPSG:27700', 'EPSG:4326')) AS geometry  -- Source CRS needs conversion and flipping
     from {{ source('geojsons', 'MOSA_2021_EW_BFC_V6_8073017919673588333.geojson') }}
 )
