bb81adc1431298d468556e09f3c5694db46ec01f
prteek <prteek@icloud.com>
2025-06-30 18:02:42 +0100

    created dim region

---
+++ backend/models/geo/dim_region/dim_region.sql
@@ -0,0 +1,12 @@
+select
+    region,
+    region_name,
+    ST_AsText(geometry) AS region_polygon_wkt,
+    ST_AsText(ST_Centroid(geometry)) AS region_centroid_wkt
+from (
+    select
+        lower(RGN24CD) AS region,
+        lower(RGN24NM) AS region_name,
+        ST_Transform(geom, 'EPSG:4326', 'EPSG:4326') AS geometry  -- Source CRS is already correct
+    from {{ source('geojsons', 'Regions_December_2024_Boundaries_EN_BFC_1195854647342073399.geojson') }}
+)
