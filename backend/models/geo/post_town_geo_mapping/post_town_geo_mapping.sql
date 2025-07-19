5dfbdc1195c79e2189962c007c6486107a1c7b7b
prteek <prteek@icloud.com>
2025-07-19 23:45:33 +0100

    fixed function reference

--- backend/models/geo/post_town_geo_mapping/post_town_geo_mapping.sql
+++ backend/models/geo/post_town_geo_mapping/post_town_geo_mapping.sql
@@ -7,14 +7,14 @@ select country
 , lsoa
 from {{ source('geo', 'dim_post_town') }} a
         inner join {{ source('geo', 'dim_msoa') }} b
-                on st_within(ST_GeomFromText(b.msoa_centroid_wkt), ST_GeomFromText(a.post_town_polygon_wkt))
+                on st_within(st_geometryfromtext(b.msoa_centroid_wkt), st_geometryfromtext(a.post_town_polygon_wkt))
         inner join {{ source('geo', 'dim_lsoa') }} c
-                on st_within(ST_GeomFromText(c.lsoa_centroid_wkt), ST_GeomFromText(b.msoa_polygon_wkt))
+                on st_within(st_geometryfromtext(c.lsoa_centroid_wkt), st_geometryfromtext(b.msoa_polygon_wkt))
         inner join {{ source('geo', 'dim_lad') }} d
-                on st_within(ST_GeomFromText(a.post_town_centroid_wkt), ST_GeomFromText(d.lad_polygon_wkt))
+                on st_within(st_geometryfromtext(a.post_town_centroid_wkt), st_geometryfromtext(d.lad_polygon_wkt))
         inner join {{ source('geo', 'dim_cua') }} e
-                on st_within(ST_GeomFromText(d.lad_centroid_wkt), ST_GeomFromText(e.cua_polygon_wkt))
+                on st_within(st_geometryfromtext(d.lad_centroid_wkt), st_geometryfromtext(e.cua_polygon_wkt))
         inner join {{ source('geo', 'dim_region') }} f
-                on st_within(ST_GeomFromText(e.cua_centroid_wkt), ST_GeomFromText(f.region_polygon_wkt))
+                on st_within(st_geometryfromtext(e.cua_centroid_wkt), st_geometryfromtext(f.region_polygon_wkt))
         inner join {{ source('geo', 'dim_country') }} g
-                on st_within(ST_GeomFromText(f.region_centroid_wkt), ST_GeomFromText(g.country_polygon_wkt))
+                on st_within(st_geometryfromtext(f.region_centroid_wkt), st_geometryfromtext(g.country_polygon_wkt))
