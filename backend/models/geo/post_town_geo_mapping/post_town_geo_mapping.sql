84c9ddf6e60f76774a69a9f86ee3622239b8fa69
prteek <prteek@icloud.com>
2025-07-19 23:43:12 +0100

    running this in athena

--- backend/models/geo/post_town_geo_mapping/post_town_geo_mapping.sql
+++ backend/models/geo/post_town_geo_mapping/post_town_geo_mapping.sql
@@ -1,14 +1,20 @@
-select cua
+select country
+, region
+, cua
 , lad
 , post_town
 , msoa
 , lsoa
-from {{ ref('dim_post_town') }} a
-        inner join {{ ref('dim_msoa') }} b
+from {{ source('geo', 'dim_post_town') }} a
+        inner join {{ source('geo', 'dim_msoa') }} b
                 on st_within(ST_GeomFromText(b.msoa_centroid_wkt), ST_GeomFromText(a.post_town_polygon_wkt))
-        inner join {{ ref('dim_lsoa') }} c
+        inner join {{ source('geo', 'dim_lsoa') }} c
                 on st_within(ST_GeomFromText(c.lsoa_centroid_wkt), ST_GeomFromText(b.msoa_polygon_wkt))
-        inner join {{ ref('dim_lad') }} d
+        inner join {{ source('geo', 'dim_lad') }} d
                 on st_within(ST_GeomFromText(a.post_town_centroid_wkt), ST_GeomFromText(d.lad_polygon_wkt))
-        inner join {{ ref('dim_cua') }} e
+        inner join {{ source('geo', 'dim_cua') }} e
                 on st_within(ST_GeomFromText(d.lad_centroid_wkt), ST_GeomFromText(e.cua_polygon_wkt))
+        inner join {{ source('geo', 'dim_region') }} f
+                on st_within(ST_GeomFromText(e.cua_centroid_wkt), ST_GeomFromText(f.region_polygon_wkt))
+        inner join {{ source('geo', 'dim_country') }} g
+                on st_within(ST_GeomFromText(f.region_centroid_wkt), ST_GeomFromText(g.country_polygon_wkt))
