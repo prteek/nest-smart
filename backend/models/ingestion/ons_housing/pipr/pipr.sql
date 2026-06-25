6352539adcebe47b7e9ce1dd400ee1c5aace8bc2
prteek <prteek@icloud.com>
2026-06-25 21:42:51 +0100

    fix: correct epoch date for PIPR time period calculation from 1899-12-30 to 1904-01-01

--- backend/models/ingestion/ons_housing/pipr/pipr.sql
+++ backend/models/ingestion/ons_housing/pipr/pipr.sql
@@ -1,4 +1,4 @@
-SELECT DATE '1899-12-30' + CAST("Time period" AS INTEGER) AS time_period
+SELECT DATE '1904-01-01' + CAST("Time period" AS INTEGER) AS time_period
 , lower("Area name") as area_name
 , case when "Rental price" = '[x]' then null
 else cast("Rental price" as double) end as average_price
